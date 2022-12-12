9e4c63fcdb3fae6be4b241baf37bdd4a5ddbe006
Шубин Алексей Константинович <AKShubin@alfabank.ru>
2022-12-13 01:59:18 +1000

    ...

--- src/shared/CardList/Card/Menu/Menu.tsx
+++ src/shared/CardList/Card/Menu/Menu.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
 import styles from './menu.css';
-import {IDropdownProps} from "../../../DropdownTemplate";
+import {Dropdown, IDropdownProps} from "../../../Dropdown";
 import {noop} from "../../../../utils/react/noop";
+import {EColors, Text} from '../../../Text'
+import {MenuItemList} from "./MenuItemList";
+import {EIcons, Icon} from "../../../Icon";
 
 
 export function Menu({children, isOpen, onClose = noop, onOpen = noop}: IDropdownProps) {
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(isOpen)
-  React.useEffect(() => setIsDropdownOpen(isOpen), [isOpen]);
-  React.useEffect(() => isDropdownOpen ? onOpen() : onClose(), [isDropdownOpen])
-
-  const handleOpen = () => {
-    if (isOpen === undefined) {
-      setIsDropdownOpen(!isDropdownOpen);
-    }
-  }
   return (
     <div className={styles.menu}>
-      <button className={styles.menuButton} onClick={handleOpen}>
-        <svg width="5" height="20" viewBox="0 0 5 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9"/>
-          <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9"/>
-          <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9"/>
-        </svg>
-      </button>
-      {isDropdownOpen && (
+      <Dropdown button={
+        <button className={styles.menuButton}>
+          <Icon name={EIcons.menu} size={5}/>
+        </button>
+      }
+      >
         <div className={styles.dropdown}>
-          <div className={styles.list} onClick={() => setIsDropdownOpen(false)}>
-            {children}
-            <div className={styles.closeButton}>Закрыть</div>
-          </div>
+          <MenuItemList postId={'1234'}/>
+          <button className={styles.closeButton}>
+            <Text mobileSize={12} size={14} color={EColors.grey66}>
+              Закрыть
+            </Text>
+          </button>
         </div>
-      )}
+      </Dropdown>
     </div>
   );
 }
