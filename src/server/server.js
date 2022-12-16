de5c77c43d3cd7efa8c15b829d2901a98371856b
Шубин Алексей Константинович <AKShubin@alfabank.ru>
2022-12-17 03:41:27 +1000

    ...

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ReactDOM from 'react-dom/server';
 import {indexTemplate} from "./indexTemplate";
 import {App} from "../App";
+import axios from "axios";
 
 const app = express();
 
@@ -14,6 +15,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/auth', (req, res) => {
+  axios.post(
+    "https://www.reddit.com/api/v1/access_token",
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    {
+      auth: {username: process.env.CLIENT_ID, password: 'vIhiH8tfH_FOCA97OwPJ2ie4ZStDzg'},
+      headers: {'Content-type': 'application/x-www-form-urlencoded'}
+    }
+  )
   res.send(
     indexTemplate(ReactDOM.renderToString(App())),
   );
